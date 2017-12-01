c88d4499fa287aae867a96474c061c2dbfd2c691
Huy Tuan <vuhuytuan89@gmail.com>
2017-12-01 13:27:32 +0700

    use ngOnInit, ghi de

--- src/app/ip.component.ts
+++ src/app/ip.component.ts
@@ -1,16 +1,23 @@
-import { Component } from "@angular/core";
+import { Component , OnInit} from "@angular/core"; // interface OnInit
 import { IpService } from "./ip.service";
+
+
 @Component({
     selector: 'app-ip',
     template: '<h3>My ip: {{ ip }} </h3>',
     providers: [IpService]
 })
 
-export class IpComponent {
+export class IpComponent implements OnInit {
+
     ip:string;
-    constructor(private ipService:IpService) {
+    //ghi de (overwrite)
+    
+    constructor(private ipService:IpService) {}
+    ngOnInit(): void {
         this.ipService.getIp()
             .then(ip=> this.ip = ip)
             .catch(err=> console.log(err))
     }
+
 }
